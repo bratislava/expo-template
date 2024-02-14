2ea93d3c89c3b14c6788eb742b32efee14961848
PavelFilo <pvlfilo@gmail.com>
2024-02-14 09:17:03 +0100

    typescript fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
+import '../global.css'
+
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView, Text, View } from 'react-native'
-import '../global.css'
 
 const RootLayout = () => {
   return (
     <SafeAreaView>
       <View className="h-full w-full items-center justify-center">
         <Text>Open up App.js to start working on your app!</Text>
+        {/* eslint-disable-next-line react/style-prop-object */}
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
