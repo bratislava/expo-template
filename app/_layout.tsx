6b426e452f0fd222bb4bc7d8f47e21796664dfae
PavelFilo <pvlfilo@gmail.com>
2024-05-03 15:07:14 +0200

    translations to have texts in one place

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,17 @@
 import '../global.css'
+import '../i18n.config.js'
 
 import { StatusBar } from 'expo-status-bar'
+import { useTranslation } from 'react-i18next'
 import { SafeAreaView, Text, View } from 'react-native'
 
 const RootLayout = () => {
+  const { t } = useTranslation()
+
   return (
     <SafeAreaView>
       <View className="h-full w-full items-center justify-center">
-        <Text>Open up App.js to start working on your app!</Text>
-        {/* eslint-disable-next-line react/style-prop-object */}
+        <Text>{t('root.heading')}</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
